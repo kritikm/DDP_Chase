7e53e44a9cd24c74ff57b00b65c068a17f243ff1
KritikM <kritik.mathur@learner.manipal.edu>
2016-05-21 01:06:10 +0530

    first upload

---
+++ Assets/Scripts/gridScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gridScript : MonoBehaviour {
+
+    
+    public Transform cellPrefab;
+    public Vector3 size;
+
+    void Start ()
+    {
+        CreateGrid();
+	}
+	
+    void CreateGrid()
+    {
+        for(int i = 0; i < size.x; i++)
+        {
+            for (int j = 0; j < size.z; j++)
+                Instantiate(cellPrefab, new Vector3(i + (float)0.1, 1, j + (float)0.1), Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/pawnScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
+
+public class pawnScript : MonoBehaviour {
+
+    public Transform pawnPrefab;
+    public int nPawns;
+    List<int> spawned = new List<int>();
+
+    void Start ()
+    {
+        for (int i = 0; i < nPawns; i++)
+        {
+            int x = (int)(Random.value * 10);
+            int z = (int)(Random.value * 4);
+
+            if (i > 0)
+            {
+                 while(spawned.Contains(10 * x + z))
+                {
+                    x = (int)(Random.value * 10);
+                    z = (int)(Random.value * 4);
+                }
+            }
+            Instantiate(pawnPrefab, new Vector3(x, 2, z), Quaternion.identity);
+            spawned.Add(10 * x + z);
+        }
+
+    }
+}
